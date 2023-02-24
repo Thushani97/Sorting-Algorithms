843e8b40298bc15c72feeb92398eb88066074022
Thushani <9thushani7@gmail.com>
2023-02-24 15:13:24 +0530

    initial commit

---
+++ src/InsertionSort.java
@@ -0,0 +1,11 @@
+//public class InsertionSort {
+//    static void insertionSort(int[] arr){
+//        for (int i = 0; i <arr.length ; i++) {
+//            for (int j =i; j <arr.length;j++){
+//                if(arr[i
+//            }
+//        }
+//    }
+//
+//}
+//// arr=[23, 4,12,9,3,-12,-3]
---
+++ src/bubbleSort.java
@@ -0,0 +1,34 @@
+class sample {
+    final int[] arr;
+    public sample(int[] i) {
+        arr = i;
+    }
+
+    public int[] Sort(){
+        bubbleSort b = new bubbleSort();
+        b.Sort(arr);
+        return arr;
+    }
+
+    public int[] Sort(int t){
+        bubbleSort b = new bubbleSort();
+        b.Sort(arr);
+        return new int[]{1};
+    }
+
+    private static class bubbleSort {
+
+        void Sort(int[] arr){
+            int n = arr.length;
+            for (int i = 0; i <n-1 ; i++) {
+                for (int j = 0; j < n-i-1; j++) {
+                    int temp = arr[j];
+                    if (arr[j]>arr[j+1]){
+                        arr[j]= arr[j+1];
+                        arr[j+1]=temp;
+                    }
+                }
+            }
+        }
+    }
+}
